a3df2fa2b61954d1fed61fe16e6531a0e80e9c8c
123-code <jose.naranjo.martinez@udla.edu.ec>
2023-12-26 11:38:06 -0500

    ___

--- HoloiOS/ViewController.swift
+++ HoloiOS/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 import Photos
 import PhotosUI
 
-class ViewController: UIViewController,PHPickerViewControllerDelegate{
+
+class MyCell: UICollectionViewCell{
+    let imageView = UIImageView()
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        addSubview(imageView)
+        
+    }
+    required init?(coder:NSCoder) {
+        fatalError()
+    }
     
+    override func layoutSubviews(){
+        super.layoutSubviews()
+        imageView.frame = bounds
+    }
+}
+
+
+class ViewController: UIViewController,PHPickerViewControllerDelegate, UICollectionViewDataSource{
+
+    
+    
+    private let collectionView:UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        let c = UICollectionView(
+        frame:.zero,collectionViewLayout:layout
+        )
+        c.backgroundColor = .red
+        c.register(MyCell.self,forCellWithReuseIdentifier:"cell")
+        return c
+        
+        
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.frame=view.bounds
         title = "New Photo Picker "
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem:.add  ,target: self, action:#selector(didTapAdd))
     }
@@ -39,12 +76,28 @@ class ViewController: UIViewController,PHPickerViewControllerDelegate{
                 guard let image  = reading as? UIImage,error == nil else{
                     return
                 }
-                print(image)
-                        
+                self.images = results.compactMap{
+                    $0.itemProvider.loadObject(ofClass: UIImage.self){
+                        result,error in guard let image = result as? UIImage,error == nil else { return nil }
+                        return image
+                    }
             }
+        } 
+    }
+    private var images = [UIImage]()
+
+    func collectionView(_ collectionView:UICollectionView,numberOfItemsInSection section:Int)->Int{
+        return images.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MyCell else {
+            fatalError()
         }
+        cell.imageView.image = images[indexPath.row]
+        return cell
     }
 }
+
 
 /*
 import AuthenticationServices
